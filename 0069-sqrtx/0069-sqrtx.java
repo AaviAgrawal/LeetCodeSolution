e5331bb27a66c3057f04ca049387281e027591bf
AaviAgrawal <157015647+AaviAgrawal@users.noreply.github.com>
2024-09-22 02:35:51 +0530

    Time: 1 ms (85.97%), Space: 40.9 MB (31.2%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+        int l = 2,r = x;
+        while (l<=r){
+            int mid = l+(r-l)/2;
+            if((long)mid*mid == (long) x) return mid;
+            else if((long)mid*mid > (long)x) r = mid-1;
+            else l = mid+1;
+        }
+        return Math.round(r);
+    }
+}
