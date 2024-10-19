857b96224755aa608d4185f0076902cda81f54b5
AaviAgrawal <157015647+AaviAgrawal@users.noreply.github.com>
2024-10-20 01:33:15 +0530

    Time: 1 ms (98.79%), Space: 42.2 MB (83.29%) - LeetHub

---
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+        int[] res = new int[s.length()];
+        int pos = -s.length();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == c) pos = i;
+            else res[i] = i-pos;
+        }
+        for(int i=pos-1;i>=0;i--){
+            if(s.charAt(i) == c) pos = i;
+            else res[i] = Math.min(res[i],pos-i);
+        }
+        return res;
+    }
+}
