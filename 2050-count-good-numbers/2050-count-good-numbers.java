a859cd08a745ec3196eaf6d75ae94dff1f5b838f
AaviAgrawal <157015647+AaviAgrawal@users.noreply.github.com>
2024-11-17 17:42:42 +0530

    Time: 0 ms (100%), Space: 40.4 MB (57.67%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -2,22 +2,21 @@ class Solution {
     private static final int MOD = 1000000007;
 
     public static int countGoodNumbers(long n) {
-        // Helper recursive function for modular exponentiation
         return (int) ((modularExponentiation(5, (n + 1) / 2) * modularExponentiation(4, n / 2)) % MOD);
     }
 
     private static long modularExponentiation(long base, long exponent) {
         if (exponent == 0) {
-            return 1; // Base case: any number to the power of 0 is 1
+            return 1;
         }
 
         long half = modularExponentiation(base, exponent / 2);
         long halfSquared = (half * half) % MOD;
 
         if (exponent % 2 == 0) {
-            return halfSquared; // If exponent is even
+            return halfSquared;
         } else {
-            return (halfSquared * base) % MOD; // If exponent is odd
+            return (halfSquared * base) % MOD;
         }
     }
 }
