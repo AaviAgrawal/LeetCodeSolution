10c37d940482bb72402d49e688c5c5188adf3f65
AaviAgrawal <157015647+AaviAgrawal@users.noreply.github.com>
2025-04-09 13:36:47 +0530

    Time: 25 ms (69.65%), Space: 56.4 MB (42.7%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int[] res = new int[nums.length];
-        Map<Integer,Integer> hm = new HashMap<>();
-        for(int i:nums){
-            hm.put(i,hm.getOrDefault(i,0)+1);
+        mergeSort(nums,0,nums.length-1);
+        return nums;
+    }
+    public void mergeSort(int[] nums,int start,int end){
+        if(start >= end) return;
+        int mid = start+(end-start)/2;
+        mergeSort(nums,start,mid);
+        mergeSort(nums,mid+1,end);
+        merge(nums,start,mid,end);
+    }
+    public void merge(int[] nums,int start,int mid,int end){
+        int[] left = Arrays.copyOfRange(nums,start,mid+1);
+        int[] right = Arrays.copyOfRange(nums,mid+1,end+1);
+        int i=0,j=0,k=start;
+        while(i<left.length && j<right.length){
+            if(left[i] <= right[j]) nums[k++] = left[i++];
+            else nums[k++] = right[j++];
         }
-        int min = nums[0];
-        int max = nums[0];
-        for(int i:nums){
-            if(i > max) max = i;
-            if(i < min) min = i;
-        }
-        int index = 0;
-        for(int i=min;i<=max;i++){
-            int count = hm.getOrDefault(i,0);
-            for(int j=0;j<count;j++){
-                res[index] = i;
-                index++;
-            }
-        }
-        return res;
+        while(i<left.length) nums[k++] = left[i++];
+        while(j<right.length) nums[k++] = right[j++];
     }
 }
